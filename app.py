a9a735e373ec0ade283728f74e8a13868407a463
Your Name <your-email@example.com>
2026-08-10 16:37:51 +0300

    Connect Flask to MongoDB

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 from flask import Flask, render_template
+
+load_dotenv()
 
 app = Flask(__name__)
 
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["vinted_egypt"]
+
+try:
+    client.admin.command("ping")
+    print("MongoDB connected successfully!")
+except Exception as e:
+    print("MongoDB connection failed:")
+    print(e)
+    
 @app.route("/")
 def home():
     return render_template("home.html")
